3d16ff130a21ce6bd165b12ceb6a29e929f6b7ad
Mikhail <44127527+Szugalew@users.noreply.github.com>
2021-03-21 16:29:45 -0400

    exposed onEmojiSelected function through config

--- lib/src/emoji_picker/search_emoji_widget.dart
+++ lib/src/emoji_picker/search_emoji_widget.dart
@@ -1,298 +1,273 @@
-import 'dart:async';
+library chat_pickers;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:giphy_client/giphy_client.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-import 'emoji_picker.dart';
+import 'src/emoji_picker/emoji_picker.dart';
+import 'src/giphy_picker/giphy_picker.dart';
+import 'src/hooks/use_page_controller.dart';
 
-/// Provides the UI for searching emojies.
-class EmojiSearchView extends StatefulWidget {
-  final Function onClose;
-  final Function(String) onSearch;
-  final Function onEmojiSelected;
-  final Function addRecentEmoji;
+export 'src/emoji_picker/emoji_picker.dart';
+export 'src/giphy_picker/giphy_picker.dart';
 
-  final int numRecommended;
-
-  final List<String> allNames;
-  final List<String> allEmojis;
-
-  final buttonMode;
-
-  final selectedCategory;
-
+class EmojiPickerConfig {
+  /// Number of columns in keyboard grid
   final int columns;
 
+  /// The background color of the keyboard
   final Color bgColor;
 
-  const EmojiSearchView(
+  /// The background color of the categories bar
+  final Color bgBarColor;
+
+  /// The color of the keyboard page indicator
+  final Color indicatorColor;
+
+  /// A list of keywords that are used to provide the user with recommended emojis in [Category.RECOMMENDED]
+  final List<String> recommendKeywords;
+
+  /// The maximum number of emojis to be recommended
+  final int numRecommended;
+
+  /// The string to be displayed if no recommendations found
+  final String noRecommendationsText;
+
+  /// The text style for the [noRecommendationsText]
+  final TextStyle noRecommendationsStyle;
+
+  /// The string to be displayed if no recent emojis to display
+  final String noRecentsText;
+
+  /// The text style for the [noRecentsText]
+  final TextStyle noRecentsStyle;
+
+  /// Determines the icon to display for each [Category]
+  final CategoryIcons categoryIcons;
+
+  /// Determines the style given to the keyboard keys
+//  final ButtonMode buttonMode;
+
+  /// Callback function for when emoji is selected
+  ///
+  /// The function returns the selected [Emoji] as well as the [Category] from which it originated
+  Function(Emoji, Category) onEmojiSelected;
+
+  EmojiPickerConfig({
+    @required this.onEmojiSelected,
+    this.columns,
+    this.bgColor,
+    this.bgBarColor,
+    this.indicatorColor = Colors.white,
+    this.recommendKeywords = const [],
+    this.numRecommended,
+    this.noRecommendationsText,
+    this.noRecommendationsStyle,
+    this.noRecentsText,
+    this.noRecentsStyle,
+    this.categoryIcons,
+//      this.buttonMode,
+  });
+}
+
+class GiphyPickerConfig {
+  /// API key for interaction with the Giphy API
+  final String apiKey;
+
+  ///
+  final String rating;
+
+  /// Language of searching gifs
+  final String lang;
+
+  ///
+  final Widget title;
+
+  ///
+  final ErrorListener onError;
+
+  ///
+  final bool showPreviewPage;
+
+  ///
+  final String searchText;
+
+  ///
+  Function(GiphyGif) onSelected;
+
+  GiphyPickerConfig(
+      {@required this.apiKey,
+      this.rating,
+      this.lang,
+      this.title,
+      this.onError,
+      this.onSelected,
+      this.showPreviewPage,
+      this.searchText});
+}
+
+class ChatPickers extends HookWidget {
+  final TextEditingController chatController;
+  final EmojiPickerConfig emojiPickerConfig;
+  final GiphyPickerConfig giphyPickerConfig;
+
+  const ChatPickers(
       {Key key,
-      this.onClose,
-      this.onSearch,
-      this.allNames,
-      this.allEmojis,
-      this.buttonMode,
-      this.numRecommended = 10,
-      this.onEmojiSelected,
-      this.addRecentEmoji,
-      this.selectedCategory,
-      this.columns,
-      this.bgColor})
+      this.chatController,
+      this.emojiPickerConfig,
+      @required this.giphyPickerConfig})
       : super(key: key);
 
   @override
-  _EmojiSearchViewState createState() => _EmojiSearchViewState();
-}
-
-class _EmojiSearchViewState extends State<EmojiSearchView> {
-  final _textController = TextEditingController();
-  final _scrollController = ScrollController();
-  final _repoController = StreamController<List>();
-
-  List<Recommended> recommendedEmojis = [];
-
-  @override
-  void initState() {
-    // initiate search on next frame (we need context)
-    Future.delayed(Duration.zero, () {
-      //_search();
-    });
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _repoController.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    var _tabSelected = useState<int>(0);
+    PageController _pageController = usePageController(_tabSelected);
+
+    Widget gifKeyboard() {
+      return GiphyPicker.pickerGifWidget(
+          context: context,
+          apiKey: giphyPickerConfig.apiKey,
+          onClose: () {},
+          onSelected: (gif) {
+            // todo: upload gif to chat
+            ///_uploadGif(gif.images.original.url);
+
+            giphyPickerConfig.onSelected(gif);
+
+            // show back keyboard
+            FocusScope.of(context).unfocus(); //?
+          });
+    }
+
+    Widget buildSticker() {
+      return EmojiPicker(
+        //context: context,
+        rows: 150,
+        columns: emojiPickerConfig.columns,
+        buttonMode: ButtonMode.MATERIAL,
+        numRecommended: emojiPickerConfig.numRecommended,
+        bgBarColor: emojiPickerConfig.bgBarColor,
+        bgColor: emojiPickerConfig.bgColor,
+        indicatorColor: emojiPickerConfig.indicatorColor,
+        onEmojiSelected: (emoji, category) {
+          // setState(() {
+          //chatController.text += emoji.emoji;
+          //});
+          emojiPickerConfig.onEmojiSelected(emoji, category);
+          // print(_messageText);
+        },
+      );
+    }
+
+    final pages = [
+      buildSticker(),
+      gifKeyboard(),
+//      Container(
+//        child: Text("Stickers"),
+//      )
+    ];
+
     return Container(
+      color: emojiPickerConfig.bgBarColor,
       child: Column(
         children: <Widget>[
-          Expanded(child: _results()),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              controller: _textController,
-              decoration: InputDecoration(
-                hintText: "Search Emojies",
-  //              hintStyle: TextStyle(color: Colors.grey),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    Icons.keyboard,
-   //                 color: Colors.white,
-                  ),
-                  onPressed: widget.onClose,
-                ),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _search(value);
-                });
-              },
+          Expanded(
+            child: PageView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: _pageController,
+              children: pages,
             ),
           ),
+          Container(
+            height: 35,
+            child: Stack(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      color: _tabSelected.value == 0
+                          ? Colors.white
+                          : Colors.grey[500],
+                      icon: Icon(Icons.insert_emoticon),
+                      onPressed: () {
+                        _pageController.animateToPage(0,
+                            duration: Duration(milliseconds: 200),
+                            curve: Curves.linear);
+                      },
+                    ),
+                    IconButton(
+                      color: _tabSelected.value == 1
+                          ? Colors.white
+                          : Colors.grey[500],
+                      icon: Icon(MdiIcons.gif),
+                      onPressed: () {
+                        _pageController.animateToPage(1,
+                            duration: Duration(milliseconds: 200),
+                            curve: Curves.linear);
+                      },
+                    ),
+//                    IconButton(
+//                      color:
+//                          _tabSelected == 2 ? Colors.white : Colors.grey[500],
+//                      icon: Icon(MdiIcons.sticker),
+//                      onPressed: () {
+//                        _pageController.animateToPage(2,
+//                            duration: Duration(milliseconds: 200),
+//                            curve: Curves.linear);
+//                      },
+//                    ),
+                  ],
+                ),
+
+//              Container(
+//                width: MediaQuery.of(context).size.width,
+//                //padding: const EdgeInsets.symmetric(horizontal: 128.0),
+//                child: Container(
+//                  constraints: BoxConstraints.tight(Size(_tabs.length * 20.toDouble(), 40)),
+//                  child: TabBar(
+//                    unselectedLabelColor: Colors.grey[500],
+//                    labelColor: Colors.white,
+//                    tabs: _tabs,
+//                    controller: _tabController,
+//                    indicatorColor: Colors.white,
+//                    indicatorWeight: 0.01,
+//                    indicatorSize: TabBarIndicatorSize.label,
+//                    ),
+//                ),
+//              ),
+                Positioned(
+                  right: 35 / 4,
+                  bottom: 35 / 4,
+                  child: Column(
+                    children: <Widget>[
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          child: Icon(
+                            MdiIcons.backspace,
+                            size: 18,
+                            color: Colors.white,
+                          ),
+                          onTap: () {
+                            try {
+                              chatController.text = chatController.text
+                                  .substring(0, chatController.text.length - 2);
+                            } catch (e) {
+                              chatController.clear();
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
   }
-
-  void _search(String t) {
-    recommendedEmojis.clear();
-
-    var recommendKeywords = [t];
-
-    if (t.isEmpty) return;
-
-    widget.allNames.forEach((name) {
-      int numSplitEqualKeyword = 0;
-      int numSplitPartialKeyword = 0;
-
-      recommendKeywords.forEach((keyword) {
-        if (name.toLowerCase() == keyword.toLowerCase()) {
-          recommendedEmojis.add(Recommended(
-              name: name,
-              emoji: widget.allEmojis[widget.allNames.indexOf(name)],
-              tier: 1));
-        } else {
-          List<String> splitName = name.split(" ");
-
-          splitName.forEach((splitName) {
-            if (splitName.replaceAll(":", "").toLowerCase() ==
-                keyword.toLowerCase()) {
-              numSplitEqualKeyword += 1;
-            } else if (splitName
-                .replaceAll(":", "")
-                .toLowerCase()
-                .contains(keyword.toLowerCase())) {
-              numSplitPartialKeyword += 1;
-            }
-          });
-        }
-      });
-
-      if (numSplitEqualKeyword > 0) {
-        if (numSplitEqualKeyword == name.split(" ").length) {
-          recommendedEmojis.add(Recommended(
-              name: name,
-              emoji: widget.allEmojis[widget.allNames.indexOf(name)],
-              tier: 1));
-        } else {
-          recommendedEmojis.add(Recommended(
-              name: name,
-              emoji: widget.allEmojis[widget.allNames.indexOf(name)],
-              tier: 2,
-              numSplitEqualKeyword: numSplitEqualKeyword,
-              numSplitPartialKeyword: numSplitPartialKeyword));
-        }
-      } else if (numSplitPartialKeyword > 0) {
-        recommendedEmojis.add(Recommended(
-            name: name,
-            emoji: widget.allEmojis[widget.allNames.indexOf(name)],
-            tier: 3,
-            numSplitPartialKeyword: numSplitPartialKeyword));
-      }
-    });
-
-    recommendedEmojis.sort((a, b) {
-      if (a.tier < b.tier) {
-        return -1;
-      } else if (a.tier > b.tier) {
-        return 1;
-      } else {
-        if (a.tier == 1) {
-          if (a.name.split(" ").length > b.name.split(" ").length) {
-            return -1;
-          } else if (a.name.split(" ").length < b.name.split(" ").length) {
-            return 1;
-          } else {
-            return 0;
-          }
-        } else if (a.tier == 2) {
-          if (a.numSplitEqualKeyword > b.numSplitEqualKeyword) {
-            return -1;
-          } else if (a.numSplitEqualKeyword < b.numSplitEqualKeyword) {
-            return 1;
-          } else {
-            if (a.numSplitPartialKeyword > b.numSplitPartialKeyword) {
-              return -1;
-            } else if (a.numSplitPartialKeyword < b.numSplitPartialKeyword) {
-              return 1;
-            } else {
-              if (a.name.split(" ").length < b.name.split(" ").length) {
-                return -1;
-              } else if (a.name.split(" ").length > b.name.split(" ").length) {
-                return 1;
-              } else {
-                return 0;
-              }
-            }
-          }
-        } else if (a.tier == 3) {
-          if (a.numSplitPartialKeyword > b.numSplitPartialKeyword) {
-            return -1;
-          } else if (a.numSplitPartialKeyword < b.numSplitPartialKeyword) {
-            return 1;
-          } else {
-            return 0;
-          }
-        }
-      }
-
-      return 0;
-    });
-
-    if (recommendedEmojis.length > widget.numRecommended) {
-      recommendedEmojis =
-          recommendedEmojis.getRange(0, widget.numRecommended).toList();
-    }
-  }
-
-  Widget _results() {
-    return Container(
-      color: widget.bgColor,
-      child: GridView.count(
-        shrinkWrap: true,
-        primary: true,
-        crossAxisCount: widget.columns,
-        children: List.generate(recommendedEmojis.length, (index) {
-          if (index < recommendedEmojis.length) {
-            switch (widget.buttonMode) {
-              case ButtonMode.MATERIAL:
-                return Center(
-                    child: FlatButton(
-                  padding: EdgeInsets.all(0),
-                  child: Center(
-                    child: Text(
-                      recommendedEmojis[index].emoji,
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  onPressed: () {
-                    Recommended recommended = recommendedEmojis[index];
-                    widget.onEmojiSelected(
-                        Emoji(name: recommended.name, emoji: recommended.emoji),
-                        widget.selectedCategory);
-                    widget.addRecentEmoji(Emoji(
-                        name: recommended.name, emoji: recommended.emoji));
-                  },
-                ));
-                break;
-              case ButtonMode.CUPERTINO:
-                return Center(
-                    child: CupertinoButton(
-                  pressedOpacity: 0.4,
-                  padding: EdgeInsets.all(0),
-                  child: Center(
-                    child: Text(
-                      recommendedEmojis[index].emoji,
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  onPressed: () {
-                    Recommended recommended = recommendedEmojis[index];
-                    widget.onEmojiSelected(
-                        Emoji(name: recommended.name, emoji: recommended.emoji),
-                        widget.selectedCategory);
-                    widget.addRecentEmoji(Emoji(
-                        name: recommended.name, emoji: recommended.emoji));
-                  },
-                ));
-
-                break;
-              default:
-                return Container();
-                break;
-            }
-          } else {
-            return Container();
-          }
-        }),
-      ),
-    );
-  }
-
-//
-//  Future _search({String term = ''}) async {
-//    // skip search if term does not match current search text
-//    if (term != _textController.text) {
-//      return;
-//    }
-//
-//    try {
-//      // search, or trending when term is empty
-//
-//      // scroll up
-//      if (_scrollController.hasClients) {
-//        _scrollController.jumpTo(0);
-//      }
-//      _repoController.add(["repo"]);
-//    } catch (error) {
-//      _repoController.addError(error);
-//    }
-//  }
 }
